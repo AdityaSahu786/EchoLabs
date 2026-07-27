e6fdc5dca050a2a1d3bdaafa06c177affbab1bf2
Aditya Sahu <sahuadi786183@gmail.com>
2026-07-27 16:45:03 +0530

    chore: fix dashboard header

--- src/features/dashboard/views/dashboard-view.tsx
+++ src/features/dashboard/views/dashboard-view.tsx
@@ -1,9 +1,16 @@
+import { PageHeader } from "@/components/ui/page-header";
 import { HeroPattern } from "@/features/dashboard/components/hero-pattern";
+import { DashboardHeader } from "../components/dashboard-header";
 
 export function DashboardView() {
     return (
         <div className="relative">
+            <PageHeader title="Dashboard" className="lg:hidden" />
             <HeroPattern />
+            <div className="relative space-y-8 p-4 lg:p-16">
+                <DashboardHeader />
+                
+            </div>
         </div>
     )
 }
